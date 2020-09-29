7490be9c4daf45a00fb2de300189f10a86be863e
Swetha Ganapathy subramanian <swethaganapathy2@gmail.com>
2020-09-29 15:31:30 -0700

    preview trial

--- studio/deskStructure.js
+++ studio/deskStructure.js
@@ -4,7 +4,7 @@ import EyeIcon from "part:@sanity/base/eye-icon";
 import EditIcon from "part:@sanity/base/edit-icon";
 import IframePreview from "./components/IframePreview";
 
-const previewURL = "http://localhost:8000";
+// const previewURL = "http://localhost:8000";
 // const WebPreview = ({ document }) => {
 //   const { displayed } = document;
 //   return <iframe src={url + displayed.slug.current} frameBorder={0} />;
@@ -12,6 +12,10 @@ const previewURL = "http://localhost:8000";
 
 // const localURL = "http://localhost:8000";
 // const previewURL = window.location.hostname === "localhost" ? localURL : remoteURL;
+
+const remoteURL = "https://gatsby-portfolio-preview-poc-4165823465.gtsb.io";
+const localURL = "http://localhost:8000";
+const previewURL = window.location.hostname === "localhost" ? localURL : remoteURL;
 
 const hiddenDocTypes = listItem =>
   !["category", "person", "sampleProject", "siteSettings", "page"].includes(listItem.getId());
